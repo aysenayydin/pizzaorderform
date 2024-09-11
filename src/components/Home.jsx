977844f3d9b54cab7c0f8105044183f2f71aa664
Aysen Aydin <aysen904@gmail.com>
2024-09-11 17:07:19 +0300

    change vısuals

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,11 @@ import Footer from "./Footer";
 function Home() {
   return (
     <>
-      <div className="h-screen flex items-end bg-red w-[100%] flex-col justify-end items-center">
+      <div className="h-[1080px] flex items-end bg-red w-[100%] flex-col justify-end items-center">
         <div className="bg-[url('/pizza-home.png')] bg-bottom md:bg-contain bg-cover bg-no-repeat md:h-full h-1/3 absolute flex justify-center md:w-[1363px] w-full">
-          <img src="/spices.png" className="absolute h-[85%] hidden md:block" />
+          <img src="/spices.png" className="absolute top-60 hidden md:block" />
         </div>
-        <div className="flex flex-col items-center mt-52 absolute top-0  text-white">
+        <div className="flex flex-col items-center mt-14 absolute top-0  text-white">
           <h1 className="font-londrina text-5xl">Teknolojik Yemekler</h1>
           <h2 className="font-roboto font-light text-7xl text-center leading-[5.5rem] mt-10 w-[398px] md:w-full">
             KOD ACIKTIRIR <br /> PİZZA, DOYURUR
