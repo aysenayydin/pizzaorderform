4bf58f3e0d4a3e2a766aa9f34b032ac7ff3d6d21
Aysen Aydin <aysen904@gmail.com>
2024-09-13 02:36:06 +0300

    change form first section

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -23,6 +23,7 @@ function Form() {
     "Jalepeno",
     "Kekik",
   ];
+
   const errors = {
     sizeError: "Lütfen boyut seçimi yapınız.",
     breadError: "Lütfen hamur kalınlığını seçiniz.",
@@ -41,7 +42,7 @@ function Form() {
   const [error, setError] = useState({});
   const [isValid, setIsValid] = useState(false);
   const [touch, setTouch] = useState({
-   boyut: false,
+    boyut: false,
     hamur: false,
     malzeme: false,
     isim: false,
@@ -167,183 +168,185 @@ function Form() {
     <>
       <Header />
       <form onSubmit={handleSubmit}>
-        <div className="flex flex-col items-center">
-          <div className="w-[532px] font-barlow">
+        <div className="flex flex-col items-center bg-[#FAF7F2]">
+          <div className=" w-[532px] font-barlow mb-10">
+            <img
+              src="assets/Iteration-2-aseets/pictures/form-banner.png"
+              alt=""
+            />
             <h2 className=" leading-7 font-semibold text-2xl mt-8 h-14">
               Position Absolute Acı Pizza
             </h2>
-            <div>
-              <div className="flex gap-80 items-center">
-                <div>
-                  <span className="text-3xl leading-9 font-bold ">85.50₺</span>
-                </div>
-                <div className="flex gap-14">
-                  <span>4.9</span>
-                  <span>(200)</span>
-                </div>
-              </div>
-              <div className="mt-4">
-                Frontent Dev olarak hala position:absolute kullanıyorsan bu çok
-                acı pizza tam sana göre. Pizza, domates, peynir ve genellikle
-                çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel
-                olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen,
-                genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan
-                oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir
-                pizzaya bazen pizzetta denir.
-              </div>
+            <div className="flex gap-80 items-center">
               <div>
-                <div className="flex gap-32 mt-10 w-[420px] ">
-                  <div className="flex flex-col gap-4">
-                    <span className="font-semibold text-xl">
-                      Boyut Seç
-                      <span className="text-red ">*</span>
-                    </span>
+                <span className="text-3xl leading-9 font-bold ">85.50₺</span>
+              </div>
+              <div className="flex gap-14">
+                <span>4.9</span>
+                <span>(200)</span>
+              </div>
+            </div>
 
-                    {sizes.map((s) => (
-                      <label htmlFor={s} className="block" key={s}>
-                        <input
-                          data-cy="size"
-                          id={s}
-                          type="radio"
-                          name="size"
-                          value={s}
-                          onChange={handleSizeChange}
-                          checked={size === s}
-                          className="mr-2"
-                        />
-                        {s}
-                      </label>
-                    ))}
-                    {touch.boyut && error.sizeError !== "" && (
-                      <span className="text-red">{error.sizeError}</span>
-                    )}
-                  </div>
-                  <div className="leading-7 ">
-                    <span className=" font-semibold text-xl">
-                      Hamur Seç<span className="text-red ">*</span>
-                    </span>
+            <div className="mt-4">
+              Frontent Dev olarak hala position:absolute kullanıyorsan bu çok
+              acı pizza tam sana göre. Pizza, domates, peynir ve genellikle
+              çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak
+              odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle
+              yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan
+              İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen
+              pizzetta denir.
+            </div>
+          </div>
+          <div className="bg-white w-full flex justify-center">
+            <div className=" w-[532px] font-barlow ">
+              <div className="flex gap-32 mt-10 w-[420px] ">
+                <div className="flex flex-col gap-4">
+                  <span className="font-semibold text-xl">
+                    Boyut Seç
+                    <span className="text-red ">*</span>
+                  </span>
 
-                    <label htmlFor="bread" className="block border">
-                      <select
-                        data-cy="breadType"
-                        id="bread"
-                        name="bread"
-                        onChange={handleBreadChange}
-                        defaultValue=""
-                      >
-                        <option value="" disabled>
-                          Hamur Kalınlığı
-                        </option>
-                        {breadTypes.map((b) => (
-                          <option value={b} key={b}>
-                            {b}
-                          </option>
-                        ))}{" "}
-                      </select>
-                    </label>
-                    {touch.hamur && error.breadError !== "" && (
-                      <span className="text-red">{error.breadError}</span>
-                    )}
-                  </div>
-                </div>
-
-                <div>
-                  <div className="mt-10 font-semibold text-xl leading-6 mb-3">
-                    Ek Malzemeler
-                  </div>
-                  <h3>En Fazla 10 malzeme seçebilirsiniz. 5₺</h3>
-                </div>
-                <div className="flex flex-wrap gap-5 w-[500px] mt-10 font-bold text-base text-[#5F5F5F]">
-                  {extras.map((e) => (
-                    <label className="md:basis-36 basis-48" htmlFor={e} key={e}>
+                  {sizes.map((s) => (
+                    <label htmlFor={s} className="block" key={s}>
                       <input
-                        type="checkbox"
-                        className="mr-1"
-                        onChange={handleSelectExtras}
-                        value={e}
-                        name={e}
-                        checked={selectedExtras.includes(e)}
-                        id={e}
-                        data-cy="extras"
+                        data-cy="size"
+                        id={s}
+                        type="radio"
+                        name="size"
+                        value={s}
+                        onChange={handleSizeChange}
+                        checked={size === s}
+                        className="mr-2"
                       />
-                      {e}
+                      {s}
                     </label>
                   ))}
-                  {touch.malzeme && error.extrasError !== "" && (
-                    <span
-                      data-cy="extras-error"
-                      className="text-red font-normal"
-                    >
-                      {error.extrasError}
-                    </span>
+                  {touch.boyut && error.sizeError !== "" && (
+                    <span className="text-red">{error.sizeError}</span>
                   )}
                 </div>
-                <div className=" mt-10">
-                  <h2 className="font-semibold text-xl leading-6 mb-3">İsim</h2>
-                  <input
-                    data-cy="name"
-                    type="text"
-                    name="name"
-                    className="border w-[531px] rounded mb-2 h-10 px-2"
-                    onChange={handleNameChange}
+                <div className="leading-7 ">
+                  <span className=" font-semibold text-xl">
+                    Hamur Seç<span className="text-red ">*</span>
+                  </span>
+
+                  <label htmlFor="bread" className="block border">
+                    <select
+                      data-cy="breadType"
+                      id="bread"
+                      name="bread"
+                      onChange={handleBreadChange}
+                      defaultValue=""
+                    >
+                      <option value="" disabled>
+                        Hamur Kalınlığı
+                      </option>
+                      {breadTypes.map((b) => (
+                        <option value={b} key={b}>
+                          {b}
+                        </option>
+                      ))}{" "}
+                    </select>
+                  </label>
+                  {touch.hamur && error.breadError !== "" && (
+                    <span className="text-red">{error.breadError}</span>
+                  )}
+                </div>
+              </div>
+
+              <div>
+                <div className="mt-10 font-semibold text-xl leading-6 mb-3">
+                  Ek Malzemeler
+                </div>
+                <h3>En Fazla 10 malzeme seçebilirsiniz. 5₺</h3>
+              </div>
+              <div className="flex flex-wrap gap-5 w-[500px] mt-10 font-bold text-base text-[#5F5F5F]">
+                {extras.map((e) => (
+                  <label className="md:basis-36 basis-48" htmlFor={e} key={e}>
+                    <input
+                      type="checkbox"
+                      className="mr-1"
+                      onChange={handleSelectExtras}
+                      value={e}
+                      name={e}
+                      checked={selectedExtras.includes(e)}
+                      id={e}
+                      data-cy="extras"
+                    />
+                    {e}
+                  </label>
+                ))}
+                {touch.malzeme && error.extrasError !== "" && (
+                  <span data-cy="extras-error" className="text-red font-normal">
+                    {error.extrasError}
+                  </span>
+                )}
+              </div>
+              <div className=" mt-10">
+                <h2 className="font-semibold text-xl leading-6 mb-3">İsim</h2>
+                <input
+                  data-cy="name"
+                  type="text"
+                  name="name"
+                  className="border w-[531px] rounded mb-2 h-10 px-2"
+                  onChange={handleNameChange}
+                />
+                {touch.isim && error.nameError !== "" && (
+                  <div data-cy="name-error" className="text-red mb-10">
+                    {error.nameError}
+                  </div>
+                )}
+                <h3 className="font-semibold text-xl mb-4 leading-6">
+                  Sipariş Notu
+                </h3>
+                <div className="border w-[531px] h-14 p-2 rounded-md flex mb-9">
+                  <textarea
+                    onChange={handleNoteChange}
+                    className=" w-full "
+                    value={note}
+                    placeholder="Siparişine eklemek istediğin bir not var mı?"
                   />
-                  {touch.isim && error.nameError !== "" && (
-                    <div data-cy="name-error" className="text-red mb-10">
-                      {error.nameError}
-                    </div>
-                  )}
-                  <h3 className="font-semibold text-xl mb-4 leading-6">
-                    Sipariş Notu
-                  </h3>
-                  <div className="border w-[531px] h-14 p-2 rounded-md flex mb-9">
-                    <textarea
-                      onChange={handleNoteChange}
-                      className=" w-full "
-                      value={note}
-                      placeholder="Siparişine eklemek istediğin bir not var mı?"
-                    />
-                  </div>
-
-                  <hr className="mb-10" />
                 </div>
-                <div className="flex justify-between items-start ">
-                  <div className="flex ">
-                    <button
-                      className="w-14 h-14 flex items-center justify-center bg-yellow rounded-l-lg"
-                      onClick={decreaseQuantity}
-                    >
-                      -
-                    </button>
-                    <div className="w-14 h-14 flex items-center justify-center border-slate-200 border">
-                      {quantity}
-                    </div>
-                    <button
-                      className="w-14 h-14 flex items-center justify-center bg-yellow rounded-r-lg"
-                      onClick={increaseQuantity}
-                    >
-                      +
-                    </button>
-                  </div>
 
-                  <div className="w-[350px] font-semibold border-slate-200 border rounded-md flex flex-col justify-center mb-40 ">
-                    <OrderSummary
-                      totalSelections={totalSelections}
-                      totalPrice={totalPrice}
-                    />
-
-                    <button
-                      type="submit"
-                      data-cy="submit"
-                      className={
-                        "h-16 rounded-md w-full bg-yellow " +
-                        `${!isValid ? "opacity-50" : "opacity-100"}`
-                      }
-                      disabled={!isValid}
-                      onClick={handleSubmit}
-                    >
-                      SİPARİŞ VER
-                    </button>
+                <hr className="mb-10" />
+              </div>
+              <div className="flex justify-between items-start ">
+                <div className="flex ">
+                  <button
+                    className="w-14 h-14 flex items-center justify-center bg-yellow rounded-l-lg"
+                    onClick={decreaseQuantity}
+                  >
+                    -
+                  </button>
+                  <div className="w-14 h-14 flex items-center justify-center border-slate-200 border">
+                    {quantity}
                   </div>
+                  <button
+                    className="w-14 h-14 flex items-center justify-center bg-yellow rounded-r-lg"
+                    onClick={increaseQuantity}
+                  >
+                    +
+                  </button>
+                </div>
+
+                <div className="w-[350px] font-semibold border-slate-200 border rounded-md flex flex-col justify-center mb-40 ">
+                  <OrderSummary
+                    totalSelections={totalSelections}
+                    totalPrice={totalPrice}
+                  />
+
+                  <button
+                    type="submit"
+                    data-cy="submit"
+                    className={
+                      "h-16 rounded-md w-full bg-yellow " +
+                      `${!isValid ? "opacity-50" : "opacity-100"}`
+                    }
+                    disabled={!isValid}
+                    onClick={handleSubmit}
+                  >
+                    SİPARİŞ VER
+                  </button>
                 </div>
               </div>
             </div>
